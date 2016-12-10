1fdd81a642e8068d86701e25f1c23fa27b9dc7ed
Philip Jackson <p-jackson@live.com>
2016-12-10 15:51:33 +1300

    Fix tests

--- test/fixtures.js
+++ test/fixtures.js
@@ -4,9 +4,9 @@ import testRunner from "babel-helper-transform-fixture-test-runner";
 const fixturesDir = join(__dirname, "fixtures");
 
 testRunner(fixturesDir, "babel-preset-env", {}, {}, (fixtureOptions, test) => {
-  const presetOptions = fixtureOptions.presets[0][1];
+  const presetOptions = fixtureOptions.presets && fixtureOptions.presets[0][1];
 
-  if (presetOptions.targets && presetOptions.targets.browserslistConfigPath) {
+  if (presetOptions && presetOptions.targets && presetOptions.targets.browserslistConfigPath) {
     const absolutePath = presetOptions.targets.browserslistConfigPath.replace("<fixture_path>", test.optionsDir);
     presetOptions.targets.browserslistConfigPath = absolutePath;
   }
