db9ce163fdc0fec57732cdf81d53067ad34dc998
Artem Yavorsky <aqson@me.com>
2017-03-24 13:49:29 +0200

    git add yarn.lock after yarn install

--- scripts/yarn-install.js
+++ scripts/yarn-install.js
@@ -2,16 +2,12 @@
 
 const { exec } = require("child_process");
 
-// function to check if yarn is installed globally
 const runIfYarn = fn => {
   exec("yarn -V", error => {
     if (error === null) fn();
   });
 };
-
-const yarn = () => {
+runIfYarn(() => {
   console.log("`package.json` was changed. Running yarn...🐈");
   exec("yarn");
-};
-
-runIfYarn(yarn);
+});
