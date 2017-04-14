7f65ac70a4f4ad84c9778be4834b890dea03e41f
Artem Yavorsky <aqson@me.com>
2017-04-14 12:39:40 +0300

    Add flow to plugins.

--- src/use-built-ins-entry-plugin.js
+++ src/use-built-ins-entry-plugin.js
@@ -1,21 +1,32 @@
-function isPolyfillSource(value) {
+//@flow
+
+type Plugin = {
+  visitor: Object,
+  pre: Function,
+  post: Function,
+  name: string,
+};
+
+type RequireType = "require" | "import";
+
+function isPolyfillSource(value: string): boolean {
   return value === "babel-polyfill";
 }
 
-export default function({ types: t }) {
-  function createImportDeclaration(polyfill) {
+export default function({ types: t }: { types: Object }): Plugin {
+  function createImportDeclaration(polyfill: string): Object {
     const declar = t.importDeclaration([], t.stringLiteral(polyfill));
     declar._blockHoist = 3;
     return declar;
   }
 
-  function createRequireStatement(polyfill) {
+  function createRequireStatement(polyfill: string): Object {
     return t.expressionStatement(
       t.callExpression(t.identifier("require"), [t.stringLiteral(polyfill)]),
     );
   }
 
-  function isRequire(path) {
+  function isRequire(path: Object): boolean {
     return t.isExpressionStatement(path.node) &&
       t.isCallExpression(path.node.expression) &&
       t.isIdentifier(path.node.expression.callee) &&
@@ -25,31 +36,39 @@ export default function({ types: t }) {
       isPolyfillSource(path.node.expression.arguments[0].value);
   }
 
-  function createImport(polyfill, requireType, core) {
+  function createImport(
+    polyfill: string,
+    requireType: RequireType,
+    core: ?boolean,
+  ): Object {
     if (core) {
       polyfill = `babel-polyfill/lib/core-js/modules/${polyfill}`;
     }
 
     if (requireType === "import") {
       return createImportDeclaration(polyfill);
-    } else {
-      return createRequireStatement(polyfill);
     }
+    return createRequireStatement(polyfill);
   }
 
-  function createImports(polyfills, requireType, regenerator) {
+  function createImports(
+    polyfills: Array<string>,
+    requireType: RequireType,
+    regenerator: boolean,
+  ): Array<Object> {
     const imports = polyfills
       .filter((el, i, arr) => arr.indexOf(el) === i)
       .map(polyfill => createImport(polyfill, requireType, true));
 
-    return [
-      ...imports,
-      regenerator &&
+    if (regenerator) {
+      imports.push(
         createImport(
           "babel-polyfill/lib/regenerator-runtime/runtime",
           requireType,
         ),
-    ].filter(Boolean);
+      );
+    }
+    return imports;
   }
 
   const isPolyfillImport = {
