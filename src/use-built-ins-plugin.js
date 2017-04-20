399e5e3ff0106b9e871a8247e28eabbef8d41d24
Artem Yavorsky <aqson@me.com>
2017-04-20 14:52:20 +0300

    Update `useBuiltIns : true` warning.

--- src/use-built-ins-plugin.js
+++ src/use-built-ins-plugin.js
@@ -28,9 +28,8 @@ const modulePathMap = {
 };
 
 const getModulePath = module => {
-  return (
-    modulePathMap[module] || `babel-polyfill/lib/core-js/modules/${module}`
-  );
+  return modulePathMap[module] ||
+    `babel-polyfill/lib/core-js/modules/${module}`;
 };
 
 export default function({ types: t }) {
@@ -64,15 +63,13 @@ export default function({ types: t }) {
   }
 
   function isRequire(path) {
-    return (
-      t.isExpressionStatement(path.node) &&
+    return t.isExpressionStatement(path.node) &&
       t.isCallExpression(path.node.expression) &&
       t.isIdentifier(path.node.expression.callee) &&
       path.node.expression.callee.name === "require" &&
       path.node.expression.arguments.length === 1 &&
       t.isStringLiteral(path.node.expression.arguments[0]) &&
-      isPolyfillSource(path.node.expression.arguments[0].value)
-    );
+      isPolyfillSource(path.node.expression.arguments[0].value);
   }
 
   const addAndRemovePolyfillImports = {
@@ -83,8 +80,8 @@ export default function({ types: t }) {
       ) {
         console.warn(
           `
-When setting "useBuiltIns: true", polyfills are automatically imported when needed.
-Please remove the "import 'babel-polyfill'" call or use "useBuiltIns: 'entry'" instead.
+When setting \`useBuiltIns: 'usage'\`, polyfills are automatically imported when needed.
+Please remove the \`import 'babel-polyfill'\` call or use \`useBuiltIns: 'entry'\` instead.
 `,
         );
         path.remove();
@@ -96,8 +93,8 @@ Please remove the "import 'babel-polyfill'" call or use "useBuiltIns: 'entry'" i
           if (isRequire(bodyPath)) {
             console.warn(
               `
-When setting "useBuiltIns: true", polyfills are automatically imported when needed.
-Please remove the "require('babel-polyfill')" call or use "useBuiltIns: 'entry'" instead.
+When setting \`useBuiltIns: 'usage'\`, polyfills are automatically imported when needed.
+Please remove the \`require('babel-polyfill')\` call or use \`useBuiltIns: 'entry'\` instead.
 `,
             );
             bodyPath.remove();
