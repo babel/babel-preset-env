89f5c520288d60c58ff2906ff672c852a8b15662
Artem Yavorsky <aqson@me.com>
2017-04-27 00:43:15 +0300

    Fix force-all-transforms debug-fixtures.

--- src/debug.js
+++ src/debug.js
@@ -13,17 +13,20 @@ export const logMessage = (message, context) => {
 
 export const logPlugin = (plugin, targets, list, context) => {
   const envList = list[plugin] || {};
-  const filteredList = Object.keys(targets).reduce((a, b) => {
-    if (!envList[b] || semver.lt(targets[b], semverify(envList[b]))) {
-      a[b] = prettifyVersion(targets[b]);
-    }
-    return a;
-  }, {});
+  const filteredList = Object.keys(targets).reduce(
+    (a, b) => {
+      if (!envList[b] || semver.lt(targets[b], semverify(envList[b]))) {
+        a[b] = prettifyVersion(targets[b]);
+      }
+      return a;
+    },
+    {},
+  );
 
   const formattedTargets = JSON.stringify(filteredList)
     .replace(/\,/g, ", ")
-    .replace(/^\{/, "{ ")
-    .replace(/\}$/, " }");
+    .replace(/^\{\"/, '{ "')
+    .replace(/\"\}$/, '" }');
 
   logMessage(`${plugin} ${formattedTargets}`, context);
 };
